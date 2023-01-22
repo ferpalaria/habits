45c38bb73c6c949376ac8493dcd4da3a188fcc33
Fernanda Palaria <fernanda_palaria@terceiros.sicredi.com.br>
2023-01-22 15:23:27 -0300

    aula 04

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
-  const today = "01/21"
-
+  const today = new Date().toLocaleDateString("pt-br").slice(0,-5)
   const dayExists = nlwSetup.dayExists(today) 
 
   if (dayExists) {
@@ -18,11 +18,15 @@ function add() {
   nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
 // const data = {
 //   meditation: ["01-20", "01-21", "01-22", "01-23"],
 //   sun: ["01-19", "01-22"],
 //   food: ["01-08"],
 // }
 
-// nlwSetup.setData(data)
-// nlwSetup.load()
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
